f4e89c19833c87d190894111803e4b9e6a9a9626
Rasbats <miker@members.swis.net>
2022-08-27 10:22:54 +0100

    Update build-and-deploy.sh

--- ci/build-and-deploy.sh
+++ ci/build-and-deploy.sh
@@ -34,7 +34,7 @@ fi
 git commit -q -m "$msg"
 git push -f origin gh-pages:gh-pages
 
-git checkout build -- ./manuals
+git checkout origin build -- ./manuals
 git push -f
 git commit -m "add globe"
 git push
