fc8fddffe2bfa5a97fba46921e405dbd89619948
Rasbats <miker@members.swis.net>
2022-08-27 10:16:01 +0100

    globe and deploy

--- ci/build-and-deploy.sh
+++ ci/build-and-deploy.sh
@@ -33,3 +33,9 @@ if [[ "$orig_subject" == *full?linkcheck* ]]; then
 fi
 git commit -q -m "$msg"
 git push -f origin gh-pages:gh-pages
+
+git checkout gh-pages
+git checkout build -- ./manuals
+git push -f
+git commit -m "add globe"
+git push
